c4d976e8a0b18562c915da1f2e934fcf49f8c66e
Andrewfro <743961@student.dusd.net>
2023-03-01 09:09:07 -0800

    Finished KeyInput Scripting Assignment

---
+++ ScriptingProjectFlores/Assets/Scenes/Scripts/LinearInterpolation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+   // In this case, result = 4
+   // Mathf.Lerp function takes 3 float parameters, one represents the value to interpolate from, another represents the value to interpolate to and a final float representing how far to interpolate
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+    // Here result = (4, 5, 6)
+    // Lerp functions are used to smooth a value over time and when using Lerp, these floats are interpolated just as with Mathf.Lerp and Vector3.Lerp
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    //The change to intensity would happen per frame 
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+
+   // The change to intensity would happen per second
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5 * Time.deltaTime);
+    }
+    */
+}
